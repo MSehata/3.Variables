5671108a6f6c79fd01cc7fdf85ce074445f18396
Moloko Sehata <moloko4@live.com>
2023-10-09 11:28:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Moloko";
+            int age;
+
+            Console.WriteLine("There is a man called " + name + " Guess how old He is: ");
+            age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("You guessed that he is " + age + " years old");
+            Console.ReadLine();
+        }
+    }
+}
